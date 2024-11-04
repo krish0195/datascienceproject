447b9deb382a70b0c28ddf99f568966554e63696
krishnaraj <krish0195@gmail.com>
2024-11-04 18:42:50 +0530

    data injestion done

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from src.datascience import logger
-logger.info("welcome to custom login data science") 
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
